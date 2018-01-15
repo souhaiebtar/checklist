aca4557e058283029060495239a5a2c55eb3515b
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-01-15 23:25:24 +0100

    add item to checklist

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -19,7 +19,29 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if  indexPath.row % 5 == 0 {
+            label.text = "walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practise"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
 
 }
 
