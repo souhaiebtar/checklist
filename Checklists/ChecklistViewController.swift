3a004ae366b37e0ca9c7d9d3c41a92bb3048a2cd
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-01-21 10:46:46 +0100

    remove what it seem duplicate code

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -15,10 +15,10 @@ class ChecklistViewController: UITableViewController {
     var row3text = "Soccer practise";
     var row4text = "Eat ice cream";
     var row0checked = false
-    var row1checked = false
-    var row2checked = false
+    var row1checked = true
+    var row2checked = true
     var row3checked = false
-    var row4checked = false
+    var row4checked = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -54,31 +54,21 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         if let cell = tableView.cellForRow(at: indexPath) {
-            var isChecked = false
-            
             if indexPath.row == 0 {
                 row0checked = !row0checked
-                isChecked = row0checked
             } else if indexPath.row == 1 {
                 row1checked = !row1checked
-                isChecked = row1checked
             } else if indexPath.row == 2 {
                 row2checked = !row2checked
-                isChecked = row2checked
             } else if indexPath.row == 3 {
                 row3checked = !row3checked
-                isChecked = row3checked
             } else if indexPath.row == 4 {
                 row4checked = !row4checked
-                isChecked = row4checked
             }
             
-            if isChecked {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+            configureCheckmark(for: cell, at: indexPath)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
